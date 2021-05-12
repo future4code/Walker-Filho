14b879ec87e514351cd7ff083843e20a2c5d7dc7
WalkerFroes <walker_aragao@hotmail.com>
2021-05-12 16:51:53 -0300

    semana2-aula09

---
+++ semana2/loops/index.js
@@ -0,0 +1,47 @@
+
+//========== # Exercícios de interpretação de código =================================================================
+
+//1. O que o código abaixo está fazendo? Qual o resultado impresso no console?
+
+    // let valor = 0
+    // for(let i = 0; i < 5; i++) {
+    //   valor += i
+    // }
+    // console.log(valor)
+
+            //R: Um contador de 1 a 5. Será impresso no console "5".
+
+//2. Leia o código abaixo:
+
+    // const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+    // for (let numero of lista) {
+    //   if (numero > 18) {
+    //         console.log(numero)
+    //     }
+    // }
+    
+    //a) O que vai ser impresso no console?
+            //R: Será impresso dodos os arrays maiores que "18". Será impresso "19, 21, 23, 25, 27, 30".
+    
+    //b) Se eu quisesse acessar o **índice** de cada elemento dessa lista, 
+    //o `for...of...` é suficiente? Se sim, o que poderia ser usado para fazer isso?
+            //R:
+
+//3. Qual seria o resultado impresso no console, se o usuário digitasse o número `4` ?
+
+    // const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+    // let quantidadeAtual = 0
+    // while(quantidadeAtual < quantidadeTotal){
+    //   let linha = ""
+    //   for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+    //     linha += "0"
+    //   }
+    //   console.log(linha)
+    //   quantidadeAtual++
+    // }
+        
+            //R: Será impresso 4 linhas de 0 .
+
+//========== # Exercícios de escrita de código ========================================================================
+
+
