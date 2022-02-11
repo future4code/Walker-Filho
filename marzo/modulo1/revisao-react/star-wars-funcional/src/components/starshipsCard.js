73bce663ea5be01bd509e5be7a9e382ae117d09b
Fróes <walker_aragao@hotmail.com>
2022-02-11 17:40:00 -0300

    pagina-satrships

---
+++ marzo/modulo1/revisao-react/star-wars-funcional/src/components/starshipsCard.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import StarWarsLogo from "../img/Star-wars-logo.jpg";
+
+const StarshipsContainer = styled.div`
+
+`;
+
+const Image = styled.img`
+
+`;
+
+const starShip = sthyled.p`
+
+`;
+
+export const starshipsCard =({ name, image}) => {
+    return (
+        <StarshipsContainer><StarshiosContainer/>
+
+    )
+}
--- marzo/modulo1/revisao-react/star-wars-funcional/src/pages/starships.js
+++ marzo/modulo1/revisao-react/star-wars-funcional/src/pages/starships.js
@@ -1,3 +1,8 @@
+import axios from 'axios';
+import styled from 'styled-components';
+import { starshipsCard } from '../components/starshipsCard';
+import { useState, useEffect } from 'react';
+
 export const StarshipsPage = () => {
     return (
         <h1>Star Ships</h1>
